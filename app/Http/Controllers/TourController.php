7e962165730ab0029d6983b656db1cc6d86b2b09
shannongreenup <44000545+shannongreenup@users.noreply.github.com>
2021-06-23 00:28:15 -0500

    Update TourController.php

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -10,18 +10,18 @@ class TourController extends Controller
 {
     public function view_all_tours()
     {
-	$tours = tour::all();
+	    $tours = tour::all();
 
         return view('index', ['tours' => $tours]);
     }
 
     public function view_tours_by_location(Request $request)
     {
-	$location = $request->location;
+	    $location = $request->location;
 
-	$view_tours_location = tour::where('location', '=', $request->location)->get();
+	    $view_tours_location = tour::where('location', '=', $request->location)->get();
 
-	return view('location', ['view_tours_location' => $view_tours_location, 'location' => $location]);
+	    return view('location', ['view_tours_location' => $view_tours_location, 'location' => $location]);
     }
 
     /* public function search_tour_by_location($location)
