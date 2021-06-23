bb1dfe740225e58504f4372290d47c1b25fc69fc
rickstylz01 <rickmaya@gmail.com>
2021-06-23 14:04:44 -0500

    modified cityList file. Fixed typo

--- src/seeders/cityList.js
+++ src/seeders/cityList.js
@@ -1,7 +1,7 @@
 const cityList = require('../models/cities.json');
 
 function seedCities() {
-	for (var i = 0; i < countryList.length; i++){
+	for (var i = 0; i < cityList.length; i++){
 		Location.create({
 			name: cityList[i].name,
 			type: "city"
