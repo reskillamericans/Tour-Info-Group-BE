035da4f68afab4f9de7491ad6df5096a5fdd6b93
klovescoding <47025707+klovescoding@users.noreply.github.com>
2021-06-25 11:29:01 -0400

    Update admin.py

--- tourinfo/tourapp/admin.py
+++ tourinfo/tourapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.db import models
+from datetime import datetime
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+
+class TouristPlaces (model.Models):
+   name = models.Charfield(max_length=255)
+   location = models.CharField (max_length=255)
+   description = models.Charfield (max_length=255) 
+   def __str__(self):
+        return self.name
